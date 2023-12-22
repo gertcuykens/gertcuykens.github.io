9ec91d98a2ee6823287d2e4466fe08cf5a1bae21
Gert Cuykens <gert.cuykens@gmail.com>
2023-12-22 01:51:14 +0100

    _

---
+++ git.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+install lazygit /usr/local/bin
+
+git config --global diff.tool vimdiff
+git config --global merge.tool vimdiff
+git config --global merge.ff only
+git config --global pull.ff only
+git config --global core.excludesfile ~/.gitignore
+git config --global advice.addIgnoredFile false
+git config --global branch.autoSetupMerge always
+git config --global branch.autoSetupRebase always
+git config --global credential.helper store
+git config --global user.name "Gert Cuykens"
+git config --global user.email gert.cuykens@gmail.com
+git config --global init.defaultBranch main
+# git config --global receive.denyCurrentBranch updateInstead
+
