031a62df6aa38cd9aa26f9855ef5dae45abedf6c
Gert Cuykens <gert.cuykens@gmail.com>
2024-07-11 02:10:18 +0200

    _

--- zsh.sh
+++ zsh.sh
@@ -1,9 +1,13 @@
 #!/bin/zsh
+set -euxo pipefail
+
+apt install zsh zsh-doc
+chsh -s /bin/zsh
+curl -fsSLO https://gert.ovh/.zshrc
+
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.config/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-completions.git ~/.config/zsh-completions
 curl https://raw.githubusercontent.com/ziglang/shell-completions/master/_zig -s -o ~/.config/zsh-completions/src/_zig
-# curl -fsSLO https://gert.ovh/.zshrc
-# chsh -s /bin/zsh
 
