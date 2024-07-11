82ef9374a95eed333be42de1e5de6b96de1f5295
Gert Cuykens <gert.cuykens@gmail.com>
2024-07-11 03:33:56 +0200

    _

--- vim.sh
+++ vim.sh
@@ -18,11 +18,10 @@ vim -u NONE -c "helptags airline/doc" -c q
 vim -u NONE -c "helptags fugitive/doc" -c q
 vim -u NONE -c "helptags jedi/doc" -c q
 
+curl -fsSLO https://gert.ovh/.vimrc
 
 rm -rf ~/.fzf
 cd ~
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-
-curl -fsSLO https://gert.ovh/.vimrc
 
