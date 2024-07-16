56d5b8a70473857308e18d156f893157e5f52331
Gert Cuykens <gert.cuykens@gmail.com>
2024-07-17 00:08:24 +0200

    _

--- git.sh
+++ git.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
-set -euxo pipefail
+set -eEuxo pipefail
+
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
