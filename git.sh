9ecf8f03be17b3aea8b2f31195e18b94525e6f7a
Gert Cuykens <gert.cuykens@gmail.com>
2025-12-06 04:28:54 +0100

    _

--- git.sh
+++ git.sh
@@ -1,25 +1,45 @@
 #!/bin/zsh
 set -eEuxo pipefail
 
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-install lazygit /usr/local/bin
-
 git config --global diff.tool vimdiff
+git config --global difftool.vimdiff.cmd 'vim -d "$LOCAL" "$REMOTE"'
+git config --global difftool.prompt false
+
+git config --global merge.tool vimdiff
+git config --global mergetool.vimdiff.cmd 'vim -d "$LOCAL" "$REMOTE" "$MERGED"'
+git config --global mergetool.vimdiff.trustExitCode true
 git config --global mergetool.prompt false
 git config --global mergetool.keepBackup false
-git config --global merge.tool vimdiff
-git config --global merge.conflictstyle diff3
+
+git config --global merge.conflictstyle merge 
 git config --global merge.ff only
 git config --global pull.ff only
+
 git config --global core.excludesfile ~/.gitignore
 git config --global advice.addIgnoredFile false
-git config --global branch.autoSetupMerge always
-git config --global branch.autoSetupRebase always
+
 git config --global init.defaultBranch main
 git config --global credential.helper store
+
+git config --global receive.denyCurrentBranch updateInstead
+
 # git config --global user.name "Gert Cuykens"
 # git config --global user.email gert.cuykens@gmail.com
-# git config --global receive.denyCurrentBranch updateInstead
+
+###############################################################################
+
+# git config --global branch.autoSetupMerge always
+# git checkout -b feature --track origin/main 
+
+# git config --global branch.autoSetupRebase always
+# git pull --rebase
+
+# git config --global mergetool.vimdiff.cmd 'vim -d "$LOCAL" "$BASE" "$REMOTE" "$MERGED"'
+
+# git config --global merge.conflictstyle diff3
+
+# LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+# curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+# tar xf lazygit.tar.gz lazygit
+# install lazygit /usr/local/bin
 
--- user.sh
+++ user.sh
@@ -3,6 +3,7 @@ set -eEuxo pipefail
 
 useradd -m -s /bin/zsh <username>
 usermod -aG docker <username>
+usermod -aG systemd-journal <username>
 
 # chsh -s /bin/zsh <username>
 
