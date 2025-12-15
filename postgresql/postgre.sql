7e30e3e3bab03763b801e652a99e87d8dce86f9d
Gert Cuykens <gert.cuykens@gmail.com>
2025-12-15 19:03:22 +0100

    _

--- postgresql/postgre.sql
+++ postgresql/postgre.sql
@@ -66,39 +66,9 @@ WHERE table_schema = 'public' AND table_name = 'ir_attachment';
 
 -------------------------------------------------------------------------------
 
-SELECT
-  *,
-  pg_size_pretty(pg_column_size(id)) AS id_size,
-  pg_size_pretty(pg_column_size(name)) AS name_size,
-  pg_size_pretty(pg_column_size(description)) AS description_size,
-  pg_size_pretty(pg_column_size(res_model)) AS res_model_size,
-  pg_size_pretty(pg_column_size(res_field)) AS res_field_size,
-  pg_size_pretty(pg_column_size(res_id)) AS res_id_size,
-  pg_size_pretty(pg_column_size(company_id)) AS company_id_size,
-  pg_size_pretty(pg_column_size(type)) AS type_size,
-  pg_size_pretty(pg_column_size(url)) AS url_size,
-  pg_size_pretty(pg_column_size(public)) AS public_size,
-  pg_size_pretty(pg_column_size(access_token)) AS access_token_size,
-  pg_size_pretty(pg_column_size(db_datas)) AS db_datas_size,
-  pg_size_pretty(pg_column_size(store_fname)) AS store_fname_size,
-  pg_size_pretty(pg_column_size(file_size)) AS file_size_size,
-  pg_size_pretty(pg_column_size(checksum)) AS checksum_size,
-  pg_size_pretty(pg_column_size(mimetype)) AS mimetype_size,
-  pg_size_pretty(pg_column_size(index_content)) AS index_content_size,
-  pg_size_pretty(pg_column_size(create_uid)) AS create_uid_size,
-  pg_size_pretty(pg_column_size(create_date)) AS create_date_size,
-  pg_size_pretty(pg_column_size(write_uid)) AS write_uid_size,
-  pg_size_pretty(pg_column_size(write_date)) AS write_date_size,
-  pg_size_pretty(pg_column_size(original_id)) AS original_id_size,
-  pg_size_pretty(pg_column_size(website_id)) AS website_id_size,
-  pg_size_pretty(pg_column_size(key)) AS key_size,
-  pg_size_pretty(pg_column_size(theme_template_id)) AS theme_template_id_size
-FROM public.ir_attachment;
-
-SELECT
-  id, name, description,
-  pg_size_pretty(pg_column_size(id)::bigint) AS id_size,
+SELECT id, name, description, key, pg_size_pretty(pg_column_size(key)::bigint) AS key_size
 FROM public.ir_attachment
-ORDER BY pg_column_size(id) DESC
+WHERE key IS NOT NULL AND key <> ''
+ORDER BY pg_column_size(key) DESC
 LIMIT 10;
 
