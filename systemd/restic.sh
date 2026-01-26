f70f594583a4a4ed70ce960c735db1b4a9150a78
Gert Cuykens <gert.cuykens@gmail.com>
2026-01-26 08:45:53 +0100

    _

--- systemd/restic.sh
+++ systemd/restic.sh
@@ -3,8 +3,8 @@
 
 backup() {
     print -r -- $1 $(date +'%y-%m-%d %H:%M:%S')
-    restic backup --tag "$1" --stdin-filename "/home/odoo/$1.sql.gz" --stdin-from-command -- pg_dump -O -Z 6 "$1"
-    restic backup --tag "$1" "/home/odoo/filestore/$1"
+    restic backup --tag "$1" --stdin-filename "/home/gert/$1.sql.gz" --stdin-from-command -- pg_dump -O -Z 6 "$1"
+    restic backup --tag "$1" "/home/gert/filestore/$1"
 }
 
 # typeset -a PG
