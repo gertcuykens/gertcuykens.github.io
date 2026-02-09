c89f3a1ce4b65c562722fe4fa8709053ceb3a1ac
Gert Cuykens <gert.cuykens@gmail.com>
2026-02-10 00:32:45 +0100

    _

--- systemd/odoo.sh
+++ systemd/odoo.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
-systemctl stop odoo.service
+systemctl stop 19.service
 cd /home/odoo/19
-/home/odoo/.local/bin/uv run ./odoo-bin -c /home/odoo/odoo.conf -d $1 -u all --no-http --workers=0 --stop-after-init
-# /home/odoo/.local/bin/uv run ./odoo-bin -c /home/odoo/odoo.conf -d $1 -i edi_tools --no-http --workers=0 --stop-after-init
-# /home/odoo/.local/bin/uv run ./odoo-bin shell -c /home/odoo/odoo.conf -d $1 --no-http --workers=0
+# uv run ./odoo-bin -c /home/odoo/19.conf -d $1 -u all --no-http --workers=0 --stop-after-init
+# uv run ./odoo-bin -c /home/odoo/19.conf -d $1 -i base,web,web_enterprise --no-http --workers=0 --stop-after-init
 
+uv run ./odoo-bin shell -c /home/odoo/19.conf -d $1 --no-http --workers=0
 # self.env['ir.module.module'].search([('name', '=', 'data_merge')]).button_immediate_uninstall()
-# update ir_module_module set state='to remove' where name='data_merge';
+# self.env['ir.attachment'].search([('url', '=like', '/web/assets/%')]).unlink()
+# self.env.cr.commit()
+# exit()
 
