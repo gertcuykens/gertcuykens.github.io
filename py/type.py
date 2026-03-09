1342edb783b4c9ac097d5e58255a0d596bfd41d5
Gert Cuykens <gert.cuykens@gmail.com>
2026-03-09 03:37:02 +0100

    _

--- py/type.py
+++ py/type.py
@@ -23,13 +23,14 @@ def test_type():
     assert not issubclass(object, type)
     assert isinstance(type, type) # !!! hardcoded circular in python
 
-    assert not isinstance(int, int) # int creates a int object not a int class
-    assert isinstance(int, type)
-    assert not isinstance(42, type)
+    assert isinstance(int, object)
     assert issubclass(int, object)
+    assert isinstance(int, type)
     assert issubclass(int, int)
     assert not issubclass(int, type)
-    assert isinstance(int, object)
+    assert not isinstance(int, int) # int creates a int object not a int class
+
+    assert not isinstance(42, type)
     assert isinstance(42, int)
     assert isinstance(42, object)
     with pytest.raises(TypeError):
