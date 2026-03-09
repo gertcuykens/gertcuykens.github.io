bb12ffb35bf85b480d122b9d946d5f86b3cf2195
Gert Cuykens <gert.cuykens@gmail.com>
2026-03-09 03:18:50 +0100

    _

--- py/type.py
+++ py/type.py
@@ -7,12 +7,15 @@ class MyClass:
 
 def test_type():
     """'type' metaclass."""
-    assert isinstance(MyClass, type)
-    assert not isinstance(MyClass, MyClass)
     assert isinstance(object, type)
-    assert isinstance(type, object)
-    assert isinstance(object, object)
+    assert isinstance(type, object) # !!!
+    assert issubclass(type, object)
+    assert isinstance(object, object) # !!!
+    assert issubclass(object, object)
+    assert issubclass(type, type) # !!!
+    assert not issubclass(object, type)
     assert isinstance(type, type) # !!! hardcoded circular in python
+
     assert not isinstance(int, int) # int creates a int object not a int class
     assert isinstance(int, type)
     assert not isinstance(42, type)
@@ -22,7 +25,13 @@ def test_type():
 
 def test_object_class():
     """'object' class"""
+    assert isinstance(MyClass, type)
     assert isinstance(MyClass, object)
+    assert not isinstance(MyClass, MyClass) # MyClass is not a metaclass of itself
+    assert not issubclass(MyClass, type)
+    assert issubclass(MyClass, object)
+    assert issubclass(MyClass, MyClass)
+
     assert isinstance([], object)
     assert isinstance(int, object)
     assert isinstance(42, int)
@@ -30,13 +39,6 @@ def test_object_class():
 
 def test_subclass():
     """inheritance"""
-    assert not issubclass(MyClass, type)
-    assert issubclass(MyClass, object)
-    assert issubclass(MyClass, MyClass)
-    assert issubclass(type, type) # !!!
-    assert issubclass(type, object)
-    assert issubclass(object, object)
-    assert not issubclass(object, type)
     assert issubclass(int, object)
     assert issubclass(int, int)
     assert not issubclass(int, type)
