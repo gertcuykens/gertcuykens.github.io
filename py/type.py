d02e75e7a094d5e4224278d92c37d5b352d7e5a5
Gert Cuykens <gert.cuykens@gmail.com>
2026-03-09 04:45:48 +0100

    _

--- py/type.py
+++ py/type.py
@@ -14,38 +14,39 @@ def test_type():
     A class can only have one metaclass.
     """
 
-    assert isinstance(object, type)
-    assert isinstance(type, object)
-    assert issubclass(type, object)
     assert isinstance(object, object)
     assert issubclass(object, object)
+    assert isinstance(object, type)
+    assert issubclass(object, object)
+    assert isinstance(object, object) # !!!
     assert not issubclass(object, type)
-    assert issubclass(type, type) # !!!
+
+    assert isinstance(type, object)
+    assert issubclass(type, object)
+    assert isinstance(type, type)
+    assert issubclass(type, type)
     assert isinstance(type, type) # !!!
+    assert issubclass(type, type) # !!!
 
     assert isinstance(int, object)
     assert issubclass(int, object)
     assert isinstance(int, type)
     assert issubclass(int, int)
-    assert not issubclass(int, type)
     assert not isinstance(int, int) # int creates a int object not a int class
+    assert not issubclass(int, type)
 
     assert isinstance(MyClass, object)
     assert issubclass(MyClass, object)
     assert isinstance(MyClass, type)
     assert issubclass(MyClass, MyClass)
-    assert not issubclass(MyClass, type)
     assert not isinstance(MyClass, MyClass) # MyClass is not a metaclass of itself
+    assert not issubclass(MyClass, type)
 
     assert isinstance(42, object)
-    # assert not issubclass(42, object)
     assert not isinstance(42, type)
-    # assert not issubclass(42, 42)
-    # assert not issubclass(42, type)
-    # assert not isinstance(42, 42)
-
     assert isinstance([], object)
     assert not isinstance([], type)
+    assert isinstance(list, object)
     assert isinstance(list, type)
 
     # with pytest.raises(TypeError):
