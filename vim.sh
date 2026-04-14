9881a97dd4472a441e3fde980d1052d3809810d5
Gert Cuykens <gert.cuykens@gmail.com>
2026-04-14 02:55:34 +0200

    _

--- vim.sh
+++ vim.sh
@@ -2,21 +2,22 @@
 set -eEuxo pipefail
 
 rm -rf ~/.local/share/vim
-mkdir -p ~/.local/share/vim/pack/gert/start
-cd ~/.local/share/vim/pack/gert/start
 
-git clone --depth 1 https://tpope.io/vim/commentary
-git clone --depth 1 https://github.com/vim-airline/vim-airline airline
-git clone --depth 1 https://tpope.io/vim/fugitive
-git clone --depth 1 https://github.com/tomasiser/vim-code-dark code
+mkdir -p ~/.local/share/vim/pack/gert/start/commentary
+mkdir -p ~/.local/share/vim/pack/gert/start/airline
+mkdir -p ~/.local/share/vim/pack/gert/start/fugitive
+mkdir -p ~/.local/share/vim/pack/gert/start/code
+mkdir -p ~/.config/vim
+mkdir -p ~/.cache/vim
+
+curl -fsSL https://github.com/tpope/vim-commentary/tarball/master | tar -xz -C ~/.local/share/vim/pack/gert/start/commentary --strip-components 1
+curl -fsSL https://github.com/vim-airline/vim-airline/tarball/master | tar -xz -C ~/.local/share/vim/pack/gert/start/airline --strip-components 1
+curl -fsSL https://github.com/tpope/vim-fugitive/tarball/master | tar -xz -C ~/.local/share/vim/pack/gert/start/fugitive --strip-components 1
+curl -fsSL https://github.com/tomasiser/vim-code-dark/tarball/master | tar -xz -C ~/.local/share/vim/pack/gert/start/code --strip-components 1
+
+curl -fsSLo ~/.config/vim/vimrc https://gert.ovh/.config/vim/vimrc
 
 vim -u NONE -c "helptags commentary/doc" -c q < /dev/tty
 vim -u NONE -c "helptags airline/doc" -c q < /dev/tty
 vim -u NONE -c "helptags fugitive/doc" -c q < /dev/tty
-
-cd ~
-mkdir -p ~/.config/vim
-curl -fsSLo ~/.config/vim/vimrc https://gert.ovh/.config/vim/vimrc
-
-mkdir -p ~/.cache/vim
 
