f887009a95977d4999b033ee16229131a8e6cb52
Gert Cuykens <gert.cuykens@gmail.com>
2026-06-13 19:18:28 +0200

    _

--- zig/hello.zig
+++ zig/hello.zig
@@ -4,3 +4,7 @@ pub fn main(init: std.process.Init) !void {
     const io = init.io;
     try std.Io.File.stdout().writeStreamingAll(io, "Hello, World!\n");
 }
+
+test "simple math" {
+    try std.testing.expectEqual(@as(u8, 2), @as(u8, 1) + 1);
+}
