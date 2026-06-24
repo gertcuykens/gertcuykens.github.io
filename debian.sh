73630590201d36e40ff076932bb9e6ca1956c0f4
Gert Cuykens <gert.cuykens@gmail.com>
2026-06-24 04:43:52 +0200

    _

--- debian.sh
+++ debian.sh
@@ -8,109 +8,79 @@ set -eEuxo pipefail
 # Components: main non-free-firmware
 # Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
-# apt update
+apt update
 # apt upgrade
-# apt install ca-certificates && update-ca-certificates
-# apt install zsh vim curl git universal-ctags tree gnupg bzip2 smartmontools nvme-cli jq
+apt install -y ca-certificates && update-ca-certificates
+apt install -y zsh vim curl git tree bzip2
+# apt install smartmontools nvme-cli
 # apt autoremove --purge
 # apt clean
 
+# chsh -s /bin/zsh root
 # infocmp -x xterm-ghostty | ssh root@... -- tic -x -
 
-# chsh -s /bin/zsh root
+curl -fsSL https://astral.sh/uv/install.sh | UV_INSTALL_DIR=/usr/local/bin sh
+uv generate-shell-completion zsh > /usr/share/zsh/vendor-completions/_uv
 
-FD_VERSION=10.3.0
-curl -fsSLo ~/fd.deb https://github.com/sharkdp/fd/releases/download/v${FD_VERSION}/fd_${FD_VERSION}_amd64.deb
-dpkg -i ~/fd.deb
-rm ~/fd.deb
+JAQ_VERSION=$(curl -fsSL https://api.github.com/repos/01mf02/jaq/releases/latest | jaq -r .tag_name) || JAQ_VERSION="v3.1.0"
+JAQ_VERSION=${JAQ_VERSION#v}
+curl -fsSLo /usr/local/bin/jaq "https://github.com/01mf02/jaq/releases/download/v${JAQ_VERSION}/jaq-x86_64-unknown-linux-gnu"
+chmod +x /usr/local/bin/jaq
 
-RG_VERSION=15.1.0
-curl -fsSLo ~/ripgrep.deb https://github.com/BurntSushi/ripgrep/releases/download/${RG_VERSION}/ripgrep_${RG_VERSION}-1_amd64.deb
-dpkg -i ~/ripgrep.deb
-rm ~/ripgrep.deb
-
-BAT_VERSION=0.26.1
+BAT_VERSION=$(curl -fsSL https://api.github.com/repos/sharkdp/bat/releases/latest | jaq -r .tag_name)
+BAT_VERSION=${BAT_VERSION#v}
 curl -fsSLo ~/bat.deb https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/bat_${BAT_VERSION}_amd64.deb
 dpkg -i ~/bat.deb
 rm ~/bat.deb
 
-FZF_VERSION=0.67.0
+FD_VERSION=$(curl -fsSL https://api.github.com/repos/sharkdp/fd/releases/latest | jaq -r .tag_name)
+FD_VERSION=${FD_VERSION#v}
+curl -fsSLo ~/fd.deb https://github.com/sharkdp/fd/releases/download/v${FD_VERSION}/fd_${FD_VERSION}_amd64.deb
+dpkg -i ~/fd.deb
+rm ~/fd.deb
+
+RG_VERSION=$(curl -fsSL https://api.github.com/repos/BurntSushi/ripgrep/releases/latest | jaq -r .tag_name)
+RG_VERSION=${RG_VERSION#v}
+curl -fsSLo ~/ripgrep.deb https://github.com/BurntSushi/ripgrep/releases/download/${RG_VERSION}/ripgrep_${RG_VERSION}-1_amd64.deb
+dpkg -i ~/ripgrep.deb
+rm ~/ripgrep.deb
+
+FZF_VERSION=$(curl -fsSL https://api.github.com/repos/junegunn/fzf/releases/latest | jaq -r .tag_name)
+FZF_VERSION=${FZF_VERSION#v}
 curl -fsSLo ~/fzf.tgz https://github.com/junegunn/fzf/releases/download/v${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz
-tar -C ~ -xzf ~/fzf.tgz fzf
-install -D -m 0755 ~/fzf /usr/local/bin
-rm -f ~/fzf.tgz ~/fzf
+tar -xzf ~/fzf.tgz -C /usr/local/bin "fzf"
+chown root:root /usr/local/bin/fzf
+rm ~/fzf.tgz
 fzf --zsh > /usr/share/zsh/vendor-completions/_fzf
 
-RESTIC_VERSION=0.18.1
+RESTIC_VERSION=$(curl -fsSL https://api.github.com/repos/restic/restic/releases/latest | jaq -r .tag_name)
+RESTIC_VERSION=${RESTIC_VERSION#v}
 curl -fsSLo ~/restic.bz2 https://github.com/restic/restic/releases/download/v${RESTIC_VERSION}/restic_${RESTIC_VERSION}_linux_amd64.bz2
 bunzip2 ~/restic.bz2
 install -D -m 0755 ~/restic /usr/local/bin
 rm ~/restic
 restic generate --zsh-completion /usr/share/zsh/vendor-completions/_restic
 
-# UV_VERSION=0.11.16
-# curl -fsSLo ~/uv.tgz https://github.com/astral-sh/uv/releases/download/${UV_VERSION}/uv-x86_64-unknown-linux-gnu.tar.gz
-# tar -C ~ -xzf ~/uv.tgz
-# install -D -m 0755 ~/uv-x86_64-unknown-linux-gnu/uv /usr/local/bin
-# install -D -m 0755 ~/uv-x86_64-unknown-linux-gnu/uvx /usr/local/bin
-# rm -rf ~/uv.tgz ~/uv-x86_64-unknown-linux-gnu
-curl -LsSf https://astral.sh/uv/install.sh | UV_INSTALL_DIR=/usr/local/bin sh
-uv generate-shell-completion zsh > /usr/share/zsh/vendor-completions/_uv
+NGINX_VERSION=$(curl -fsSL https://api.github.com/repos/nginx/nginx-prometheus-exporter/releases/latest | jaq -r .tag_name)
+NGINX_VERSION=${NGINX_VERSION#v}
+curl -fsSLo ~/nginx_exporter.tgz "https://github.com/nginx/nginx-prometheus-exporter/releases/download/v${NGINX_VERSION}/nginx-prometheus-exporter_${NGINX_VERSION}_linux_amd64.tar.gz"
+tar -xzf ~/nginx_exporter.tgz -C /usr/local/bin nginx-prometheus-exporter
+chown root:root /usr/local/bin/nginx-prometheus-exporter
+mv /usr/local/bin/nginx-prometheus-exporter /usr/local/bin/nginx_exporter
+rm ~/nginx_exporter.tgz
 
+NODE_VERSION=$(curl -fsSL https://api.github.com/repos/prometheus/node_exporter/releases/latest | jaq -r .tag_name)
+NODE_VERSION=${NODE_VERSION#v}
+curl -fsSLo ~/node_exporter.tgz "https://github.com/prometheus/node_exporter/releases/download/v${NODE_VERSION}/node_exporter-${NODE_VERSION}.linux-amd64.tar.gz"
+tar -xzf ~/node_exporter.tgz -C /usr/local/bin --strip-components=1 --wildcards "*/node_exporter"
+chown root:root /usr/local/bin/node_exporter
+rm ~/node_exporter.tgz
 
-LNAV_VERSION=0.14.0
-curl -fsSLo ~/lnav.zip https://github.com/tstack/lnav/releases/download/v${LNAV_VERSION}-beta2/lnav-${LNAV_VERSION}-beta2-linux-musl-x86_64.zip
-unzip ~/lnav.zip
-install -D -m 0755 ~/lnav-${LNAV_VERSION}/lnav /usr/local/bin
-rm ~/lnav.zip ~/lnav-${LNAV_VERSION}
-
-ZIG_VERSION=0.0.0
+ZIG_VERSION=$(curl -fsSL "https://codeberg.org/api/v1/repos/ziglang/zig/tags" | jaq -r '.[0].name')
+curl -fsSLo ~/zig.txz "https://ziglang.org/download/${ZIG_VERSION}/zig-x86_64-linux-${ZIG_VERSION}.tar.xz"
+tar -xf ~/zig.txz -C /usr/local/bin --strip-components=1 --wildcards "*/zig" "*/lib/*"
+chown -R root:root /usr/local/bin/zig /usr/local/bin/lib
+mv /usr/local/bin/lib /usr/local/lib/zig
+rm ~/zig.txz
 curl -fsSLo /usr/share/zsh/vendor-completions/_zig https://codeberg.org/ziglang/shell-completions/raw/branch/master/_zig
-
-# TODO: install ctags
-# mkdir -p ~/.config/ctags
-# curl -fsSLo ~/.config/ctags/py.ctags https://gert.ovh/.config/ctags/py.ctags
-# ctags --list-kinds-full=Python
-# ctags --verbose
-
-# curl -fsSL https://api.github.com/repos/sharkdp/fd/releases/latest | jq -r .tag_name
-# curl -fsSL https://api.github.com/repos/BurntSushi/ripgrep/releases/latest | jq -r .tag_name
-# curl -fsSL https://api.github.com/repos/sharkdp/bat/releases/latest | jq -r .tag_name
-# curl -fsSL https://api.github.com/repos/junegunn/fzf/releases/latest | jq -r .tag_name
-# curl -fsSL https://api.github.com/repos/restic/restic/releases/latest | jq -r .tag_name
-# curl -fsSL https://api.github.com/repos/astral-sh/uv/releases/latest | jq -r .tag_name
-# curl -fsSL https://api.github.com/repos/tstack/lnav/releases/latest | jq -r .tag_name
-
-# curl -fsSL https://rclone.org/install.sh | zsh
-
-# typeset -U fpath
-# type python3
-# whence -av python3
-# command -v python3
-
-# rm -rf /var/lib/apt/lists/*
-# rm -rf /tmp/*
-# rm -rf /var/tmp/*
-
-# smartctl --scan
-# smartctl -a /dev/nvme0
-# nvme list
-# nvme smart-log /dev/nvme0 -H
-# nvme error-log /dev/nvme0
-
-# lsblk
-# mdadm --detail /dev/md1
-# echo check > /sys/block/md1/md/sync_action # repair
-# cat /sys/block/md1/md/mismatch_cnt
-# cat /proc/mdstat
-
-# xfs_info / | grep ftype
-# xfs_scrub -v -n /
-
-# e2fsck -f /dev/md1
-
-# ip addr show ...
-# lspci -vv -s ...
-# ls -l /sys/class/net/.../device
-# ls -l /sys/firmware/efi
 
