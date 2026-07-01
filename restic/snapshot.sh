26ca5183686632c0709617eb615d1a2cf9fccca5
Gert Cuykens <gert.cuykens@gmail.com>
2026-07-01 15:05:43 +0200

    _

--- restic/snapshot.sh
+++ restic/snapshot.sh
@@ -1,14 +1,27 @@
 #!/bin/zsh
 
+sort_by_time=false
+for arg in "$@"; do
+  if [[ "$arg" == "--time" ]]; then
+    sort_by_time=true
+  fi
+done
+
 restic snapshots \
   --latest 1 \
   --group-by host,tags,paths \
   --json \
-  | jaq -r '
+  | jaq --argjson sort_by_time "$sort_by_time" -r '
       [ "Time", "Host", "Tags", "Paths", "ID" ],
       [ "----", "----", "----", "-----", "--" ],
       (
-        sort_by([.group_key.hostname, .group_key.tags, .group_key.paths])
+        sort_by(
+          if $sort_by_time then
+            [.snapshots[0].time, .group_key.hostname, .group_key.tags, .group_key.paths]
+          else
+            [.group_key.hostname, .group_key.tags, .group_key.paths]
+          end
+        )
         | .[]
         | .group_key as $gk
         | .snapshots[]
