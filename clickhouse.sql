638984ddf8c05c2c89b4812f3179e95726131ca9
Gert Cuykens <gert.cuykens@gmail.com>
2026-07-05 10:13:17 +0200

    _

--- clickhouse.sql
+++ clickhouse.sql
@@ -26,3 +26,39 @@ SELECT * FROM test_table;
 
 -- chc --queries-file /root/system/clickhouse.sql
 
+CREATE ROLE clickhouse WITH LOGIN REPLICATION PASSWORD '...';
+GRANT CONNECT ON DATABASE ... TO clickhouse;
+GRANT USAGE ON SCHEMA public TO clickhouse;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO clickhouse;
+ALTER DEFAULT PRIVILEGES FOR ROLE table_creator_role IN SCHEMA public GRANT SELECT ON TABLES TO clickhouse;
+CREATE PUBLICATION ..._ch_publication FOR TABLE public.users;
+SELECT pg_create_logical_replication_slot('..._slot', 'pgoutput');
+ALTER USER clickhouse WITH SUPERUSER;
+
+REASSIGN OWNED BY clickhouse TO postgres;
+DROP OWNED BY clickhouse;
+DROP ROLE clickhouse;
+ALTER ROLE clickhouse NOSUPERUSER;
+ALTER DEFAULT PRIVILEGES FOR ROLE table_creator_role IN SCHEMA public REVOKE SELECT ON TABLES FROM clickhouse;
+
+ALTER TABLE ... REPLICA IDENTITY FULL;
+
+--
+
+DESCRIBE TABLE postgresql('localhost:5432', 'db_name', 'table_name', 'clickhouse', '...');
+
+SET allow_experimental_database_materialized_postgresql = 1;
+CREATE DATABASE ...db_name ENGINE = MaterializedPostgreSQL('localhost:5432', 'db_name', 'clickhouse', '...'
+)SETTINGS materialized_postgresql_tables_list = '...,...';
+          -- materialized_postgresql_replication_slot = '..._slot',
+          -- materialized_postgresql_snapshot = '..._ch_publication';
+
+SELECT name, engine, metadata_path, uuid FROM system.databases WHERE name = '...';
+
+SELECT count() FROM d.t;
+
+DROP DATABASE IF EXISTS ...;
+
+SELECT slot_name, plugin, slot_type, active FROM pg_replication_slots;
+SELECT pg_drop_replication_slot('...');
+ALTER SYSTEM SET wal_level = 'replica';
