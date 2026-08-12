ccf8f384fc074df7ab728c07986f8ca37edfc1fb
Gert Cuykens <gert.cuykens@gmail.com>
2026-08-12 15:49:14 +0200

    _

--- py/lib/main.py
+++ py/lib/main.py
@@ -1,10 +1,10 @@
 import logging
 
 from fastapi import APIRouter, Depends, Request
-
-# from sqlmodel import SQLModel, select
+from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
+from lib.model import User
 from run.dependencies import get_session
 
 logger = logging.getLogger(__name__)
@@ -16,5 +16,19 @@ async def hello_world(
     request: Request, session: AsyncSession = Depends(get_session)
 ) -> dict[str, str]:
     logger.debug(request.headers)
-    # todo: Use the session to query the database
-    return {"message": "Hello, World!"}
+    user_name = "hello-world"
+    result = await session.exec(select(User).where(User.name == user_name))
+    user = result.one_or_none()
+
+    if user is None:
+        user = User(name=user_name, display_name="Hello World")
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+    elif user.display_name != "Hello World":
+        user.display_name = "Hello World"
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+    return {"message": f"{user.display_name or user.name}!"}
